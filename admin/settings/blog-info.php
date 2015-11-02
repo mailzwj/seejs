cf8d22454b38ebbb54944876d032e3a4dabf58ff
mailzwj <mailzwj@126.com>
2015-11-02 11:11:19 +0800

    Update blog-info.php

--- admin/settings/blog-info.php
+++ admin/settings/blog-info.php
@@ -25,17 +25,17 @@
     <form action="./info-save.php" method="post" id="J_InfoForm">
         <input type="hidden" name="id" value="<?php echo $data ? $data['id'] : ''; ?>">
         <div class="ed-row">
-            <label for="J_InfoLogo" class="label">分类名称</label><input type="text" name="logo" id="J_InfoLogo" class="input" placeholder="请上传博客LOGO(推荐：150x165)" value="<?php echo $data ? $data['logo'] : ''; ?>">
+            <label for="J_InfoLogo" class="label">博客LOGO</label><input type="text" name="logo" id="J_InfoLogo" class="input" placeholder="请上传博客LOGO(推荐：150x165)" value="<?php echo $data ? $data['logo'] : ''; ?>">
             <div class="button upload">
                 <span class="icon-upload40"></span>
                 <input type="file" name="image" id="J_UploadLogo" class="up-image">
             </div>
         </div>
         <div class="ed-row">
-            <label for="J_InfoName" class="label">分类名称</label><input type="text" name="sitename" id="J_InfoName" class="input" placeholder="请输入博客名称" value="<?php echo $data ? $data['sitename'] : ''; ?>">
+            <label for="J_InfoName" class="label">博客名称</label><input type="text" name="sitename" id="J_InfoName" class="input" placeholder="请输入博客名称" value="<?php echo $data ? $data['sitename'] : ''; ?>">
         </div>
         <div class="ed-row">
-            <label for="J_InfoSub" class="label">分类名称</label><input type="text" name="subname" id="J_InfoSub" class="input" placeholder="请输入副标题" value="<?php echo $data ? $data['subname'] : ''; ?>">
+            <label for="J_InfoSub" class="label">博客副标题</label><input type="text" name="subname" id="J_InfoSub" class="input" placeholder="请输入副标题" value="<?php echo $data ? $data['subname'] : ''; ?>">
         </div>
         <div class="ed-row btn-row">
             <input type="submit" value="保 存" id="J_SaveInfo" class="button">
